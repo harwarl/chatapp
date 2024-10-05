ded5e0c5ed5a64c4be342f755418b217c6145d8a
harwarl <harwarl87@gmail.com>
2024-10-05 23:22:46 +0100

    feat: client setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      screens: {
+        sm: "425px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1440px",
+      },
+    },
+  },
+  plugins: [],
+};
