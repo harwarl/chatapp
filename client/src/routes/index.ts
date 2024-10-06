db1f628875c0e1a661fdce47a1902c8ebd0d422f
harwarl <harwarl87@gmail.com>
2024-10-06 21:29:37 +0100

    route interfaces

--- client/src/routes/index.ts
+++ client/src/routes/index.ts
@@ -1,10 +1,40 @@
+import { ComponentType } from "react";
+
+import HomePage from "../pages/Home/HomePage";
+import SigninPage from "../pages/Signin/SigninPage";
+import SignUpPage from "../pages/Signup/SignUpPage";
+import ProfilePage from "../pages/Profile/ProfilePage";
+import AddChannelPage from "../pages/Add-Channel/AddChannelPage";
+import ChannelPage from "../pages/Channel/ChannelPage";
+import ChannelsPage from "../pages/Channels/ChannelsPage";
+import UpdateChannelPage from "../pages/Update-channel/UpdateChannelPage";
+
 export enum Paths {
   HOME = "/",
-  SIGNUP = "/register",
-  SIGNIN = "/login",
+  SIGNUP = "/sign-up",
+  SIGNIN = "/sign-in",
   PROFILE = "/profile",
   CHANNELS = "/channels",
   CHANNEL = "/channels/:id",
   ADD_CHANNEL = "/channels/add",
   UPDATE_CHANNEL = "/channels/:id/update",
 }
+
+export interface IRoute {
+  path: Paths;
+  Component: ComponentType;
+}
+
+export const publicRoutes: IRoute[] = [
+  { path: Paths.HOME, Component: HomePage },
+  { path: Paths.SIGNIN, Component: SigninPage },
+  { path: Paths.SIGNUP, Component: SignUpPage },
+];
+
+export const authRoutes: IRoute[] = [
+  { path: Paths.PROFILE, Component: ProfilePage },
+  { path: Paths.ADD_CHANNEL, Component: AddChannelPage },
+  { path: Paths.CHANNEL, Component: ChannelsPage },
+  { path: Paths.CHANNELS, Component: ChannelPage },
+  { path: Paths.UPDATE_CHANNEL, Component: UpdateChannelPage },
+];
