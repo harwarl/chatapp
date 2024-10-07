1af25449617f05327ed5815d1b07516282964b61
harwarl <harwarl87@gmail.com>
2024-10-07 14:48:25 +0100

    tailwind css setup

---
+++ client/src/components/ContentArea/ContentArea.tsx
@@ -0,0 +1,15 @@
+import { ReactNode, FC } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+
+const ContentArea: FC<Props> = ({ children }) => {
+  return (
+    <div className="xl:col-span-5 md:col-span-3 h-full overflow-y-auto bg-black">
+      {children}
+    </div>
+  );
+};
+
+export default ContentArea;
--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -1,8 +1,24 @@
-import { PropsWithChildren, FC } from "react";
-import { Container } from "@mui/material";
+import { FC, ReactNode } from "react";
+import { useLocation } from "react-router-dom";
+import ContentArea from "../ContentArea";
 
-const Layout: FC<PropsWithChildren> = ({ children }) => {
-  return <Container component="main">{children}</Container>;
+type Props = {
+  children: ReactNode;
+};
+
+const Layout: FC<Props> = ({ children }) => {
+  const location = useLocation();
+  if (location.pathname === "/login" || location.pathname === "/register") {
+    return <>{children}</>;
+  }
+
+  return (
+    <div>
+      <div>
+        <ContentArea>{children}</ContentArea>
+      </div>
+    </div>
+  );
 };
 
 export default Layout;
