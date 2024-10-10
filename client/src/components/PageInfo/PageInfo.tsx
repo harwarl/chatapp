5a6d7048af0defa1adc4d64557e23feff8a24a37
harwarl <harwarl87@gmail.com>
2024-10-10 23:30:52 +0100

    chore: home page view

--- client/src/components/PageInfo/PageInfo.tsx
+++ client/src/components/PageInfo/PageInfo.tsx
@@ -1,6 +1,9 @@
 import { FC } from "react";
 import { User } from "../../utils/types";
 import { useLocation, useNavigate } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import { RxDotsVertical } from "react-icons/rx";
 
 type Props = {
   image?: string;
@@ -10,14 +13,38 @@ type Props = {
 };
 
 const PageInfo: FC<Props> = ({ image, name, participants, isChannel }) => {
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    return (
+  const handleClick = () => {
+    if (location.pathname === "/chat")
+      return navigate("/channel", {
+        state: { channelId: location.state.channelId },
+      });
+
+    navigate("/chat", { state: { channelId: location.state.channelId } });
+  };
+
+  return (
+    <div>
+      <FaArrowLeft onClick={() => navigate(-1)} />
+      {image && <LazyLoadImage src={image} alt="'channel-pp" effect="blur" />}
+      <div>
+        <p>{name}</p>
+        <p>{name}</p>
+        {participants && <span>Participants:</span>}
+        {participants?.length! > 0 &&
+          participants?.map((participants) => {
+            return <span key={participants.id}>{participants.username}</span>;
+          })}
+      </div>
+      {isChannel && participants !== null && (
         <div>
-            
+          <RxDotsVertical onClick={handleClick} />
         </div>
-    )
+      )}
+    </div>
+  );
 };
 
 export default PageInfo;
